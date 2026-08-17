467fbacae56ccd238cfe41e685716d3bdd72d207
radek-ops <radek.network@gmail.com>
2026-08-17 14:30:41 +0200

    add new body  backgraund anf new fonts

--- js/models/final-boss.class.js
+++ js/models/final-boss.class.js
@@ -7,13 +7,10 @@ class FinalBoss extends Moveables {
         super();
         this.x = 2500;
         this.y = -900;
-
         this.width = 2000;
         this.height = 2000;
-
         this.addFinalBossImages();
         this.saveImages(this.IMAGES_IDLE);
-
         this.img = this.imageCache[this.IMAGES_IDLE[0]];
         this.startAnimation();
     }
@@ -25,7 +22,6 @@ class FinalBoss extends Moveables {
             this.IMAGES_IDLE.push(`./img/PNG/Enemy_Characters/Enemy_Character10/Idle/Idle_${number}.png`);
         }
     }
-
 
     startAnimationUpdate() {
         this.frameCounter++;
