801f2a7afc592484a5414eae0e3dd564c5dabb21
radek-ops <radek.network@gmail.com>
2026-08-17 14:30:42 +0200

    add js

--- js/models/background.class.js
+++ js/models/background.class.js
@@ -3,6 +3,14 @@ class Background extends Moveables {
 
 
 
+  /**
+   * Creates a background image.
+   * @param {string} imgPath - The image path
+   * @param {number} x - The x position
+   * @param {number} y - The y position
+   * @param {number} width - The width
+   * @param {number} height - The height
+   */
   constructor(imgPath, x, y = 0, width = 1283, height = 720) {
     super();
     this.loadImages(imgPath);
